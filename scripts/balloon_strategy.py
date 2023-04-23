d4141dd162329fc3f4f82f7032032ea074d0031c
Ranjani <Ranjani@RANJANISLAPTOP>
2023-04-23 13:34:46 -0400

    tests

--- scripts/balloon_strategy.py
+++ scripts/balloon_strategy.py
@@ -610,6 +610,29 @@ class BalloonStrategy(object):
         self.stats_start_time = now
 
     def run(self):
+    
+        # arm the drone
+        vehicle.armed   = True
+        
+        while not vehicle.armed:
+            print(" Waiting for arming...")
+            time.sleep(1)
+        
+        # takeoff to 2m
+        # Copter should arm in GUIDED mode
+        vehicle.mode    = VehicleMode("GUIDED")
+        vehicle.simple_takeoff(2)
+        
+        # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        #  after Vehicle.simple_takeoff will execute immediately).
+        while True:
+            print " Altitude: ", vehicle.location.global_relative_frame.alt
+            #Break and return from function just below target altitude.
+            if vehicle.location.global_relative_frame.alt>=3*0.95:
+                print("Reached target altitude")
+                break
+            time.sleep(1)
+
         while True:
 
             # only process images once home has been initialised
